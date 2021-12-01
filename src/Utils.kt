c30b6f058c1bafa2a8e7975a88b8fc83bff25ae2
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2021-12-01 22:13:11 +0100

    Add day 1 solutions

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,11 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines from input file as ints.
+ */
+fun readInt(name: String) = readInput(name).map { it.toInt() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
