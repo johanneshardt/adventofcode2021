426c8c20098ad3c8cf9052a88ae2939f968c12ef
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2021-12-06 20:46:46 +0100

    Improve readability of day 6 solution

--- src/day06/Solution.kt
+++ src/day06/Solution.kt
@@ -13,39 +13,41 @@ fun main() {
                     .map { it.toInt() }
                     .groupingBy { it }
                     .eachCount()
-                    .map { FishState(it.key, it.value) }
+                    .map { FishState(age = it.key, count = it.value) }
             }
             .toSet()
     }
 
-    fun part1and2(input: Set<FishState>, days: Int): Long {
-        val count: MutableList<Long> = MutableList(9) { 0 }
-        input.forEach { count[it.age] = it.count.toLong() }
-        for (i in 1..days) {
-            var bornFish: Long = 0
-            var resetFish: Long = 0
-            for ((index, numFish) in count.withIndex()) {
-                if (index == 0) {
-                    bornFish = numFish
-                    resetFish = numFish
-                    count[0] = 0
-                } else {
-                    count[index - 1] += count[index]
-                    count[index] = 0
-                }
+    fun simulateFish(input: Set<FishState>, days: Int): Long {
+        val fishCount: MutableList<Long> = MutableList(9) { 0 }
+        input.forEach { fishCount[it.age] = it.count.toLong() }
+        repeat(days) {
+            val newFish = fishCount.first()
+            fishCount.indices.drop(1).forEach {
+                fishCount[it - 1] = fishCount[it]
+                fishCount[it] = 0
             }
-            count[6] += resetFish
-            count[8] += bornFish
+            fishCount[6] += newFish
+            fishCount[8] += newFish
         }
-        return count.sum()
+        return fishCount.sum()
     }
 
+    fun part1(input: Set<FishState>, days: Int = 80): Long {
+        return simulateFish(input, days)
+    }
+
+    fun part2(input: Set<FishState>, days: Int = 256): Long {
+        return simulateFish(input, days)
+    }
+
+
     val testInput = parse(readInput("day06/test"))
-    check(part1and2(testInput, 18) == 26.toLong())
+    check(part1(testInput, 18) == 26.toLong())
 
     val input = parse(readInput("day06/input"))
-    println(part1and2(input, 80))
-    println(part1and2(input, 256))
+    println(part1(input))
+    println(part2(input))
 }
 
 data class FishState(val age: Int, val count: Int)
