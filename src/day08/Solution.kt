87b2d8467cd43df938715c591185258bde9f4f7c
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2021-12-08 22:35:08 +0100

    Refactor day 8 solution

--- src/day08/Solution.kt
+++ src/day08/Solution.kt
@@ -17,59 +17,65 @@ fun main() {
 
 
     fun mapToDigits(numbers: Patterns): Map<Pattern, Int> {
+        val patterns = numbers.list.toMutableSet()
         val mapping = mutableMapOf<Int, Pattern>()
-        val patterns = numbers.patterns.toMutableSet()
         val signalStrings = mutableMapOf<String, Set<Char>>()
-        setOf(2, 4, 3, 7).map { len ->
+
+        fun signal(key: Int): Set<Char> {
+            return mapping[key]?.signal ?: throw NoSuchElementException("No element named $key in mapping")
+        }
+
+        // Extract patterns for 1,4,7,8 since they can be detected by length
+        setOf(2, 4, 3, 7).forEach { len ->
             val match = patterns.first { it.signal.size == len }
-            patterns -= match   // TODO change this somehow
-            len to match
-        }.forEach {
             // Remap values from number of signals to the actual, displayed number
-            when (it.first) {
-                2 -> mapping += 1 to it.second
-                4 -> mapping += 4 to it.second
-                3 -> mapping += 7 to it.second
-                7 -> mapping += 8 to it.second
+            when (len) {
+                2 -> mapping += 1 to match
+                4 -> mapping += 4 to match
+                3 -> mapping += 7 to match
+                7 -> mapping += 8 to match
             }
+            // Remove the pattern we just found
+            patterns -= match
         }
 
         // Since the input structure is guaranteed, I used !! liberally in this function
-        signalStrings += "a" to (mapping[7]!!.signal subtract mapping[1]!!.signal)
-        signalStrings += "bd" to (mapping[4]!!.signal subtract mapping[1]!!.signal)
-        signalStrings += "aeg" to (mapping[8]!!.signal subtract mapping[4]!!.signal)
+        // signalStrings are helper mappings to easier identify some digits
+        signalStrings += "a" to (signal(7) subtract signal(1))
+        signalStrings += "bd" to (signal(4) subtract signal(1))
+        signalStrings += "aeg" to (signal(8) subtract signal(4))
         signalStrings += "eg" to (signalStrings["aeg"]!! subtract signalStrings["a"]!!)
 
-
+        // Identify each of the remaining digits by creating set differences with either
+        // the signalStrings or one of the solved digits
         for (p in patterns) {
             when (p.signal.size) {
-                // 0, 6, 9
+                // Identify 0, 6, 9
                 6 -> {
                     mapping += if (
-                        (p.signal subtract signalStrings["a"]!! subtract mapping[4]!!.signal) != signalStrings["eg"]!!) {
+                        (p.signal subtract signalStrings["a"]!! subtract signal(4)) != signalStrings["eg"]!!) {
                         9 to p
-                    } else if ((p.signal subtract mapping[1]!!.signal).size == 5) {
+                    } else if ((p.signal subtract signal(1)).size == 5) {
                         6 to p
-                    } else if ((p.signal subtract mapping[1]!!.signal).size == 4) {
+                    } else if ((p.signal subtract signal(1)).size == 4) {
                         0 to p
                     } else {
-                        throw IllegalStateException("Invalid input")
+                        throw IllegalStateException("Invalid signal of length 6: $p.signal")
                     }
                 }
-                // 2, 3, 5
+                // Identify 2, 3, 5
                 5 -> {
-                    mapping += if ((p.signal subtract mapping[4]!!.signal).size == 3) {
+                    mapping += if ((p.signal subtract signal(4)).size == 3) {
                         2 to p
-                    } else if ((p.signal subtract mapping[7]!!.signal).size == 2) {
+                    } else if ((p.signal subtract signal(7)).size == 2) {
                         3 to p
                     } else if ((p.signal subtract signalStrings["bd"]!!).size == 3) {
                         5 to p
                     } else {
-                        throw IllegalStateException("Invalid input")
+                        throw IllegalStateException("Invalid signal of length 5: $p.signal")
                     }
-
                 }
-                else -> throw IllegalStateException("Invalid input")
+                else -> throw IllegalStateException("Invalid signal: $p.signal")
             }
         }
 
@@ -78,22 +84,19 @@ fun main() {
 
     fun part1(input: List<Pair<Patterns, Patterns>>): Int {
         return input.sumOf { line ->
-            line.second.patterns.map { it.signal.size }.count { it in setOf(2, 4, 3, 7) }
+            line.second.list.map { it.signal.size }.count { it in setOf(2, 4, 3, 7) }
         }
     }
 
     fun part2(input: List<Pair<Patterns, Patterns>>): Int {
-        val v = input.sumOf { line ->
+        return input.sumOf { line ->
             val mapping = mapToDigits(line.first)
-            val output = line.second.patterns
-            val mapped = output.map { pattern ->
+            val patterns = line.second.list
+            patterns.map { pattern ->
                 mapping[pattern]
-            }
-            mapped.joinToString("").toInt()
+            }.joinToString("").toInt()
         }
-        return v
     }
-
 
     val testInput = parse(readInput("day08/test"))
     check(part1(testInput) == 26)
@@ -106,4 +109,4 @@ fun main() {
 
 data class Pattern(val signal: Set<Char>)
 
-data class Patterns(val patterns: List<Pattern>)
+data class Patterns(val list: List<Pattern>)
